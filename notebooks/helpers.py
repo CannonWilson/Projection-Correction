9bb149fece3214fb926df3c6949b28825f7c1a4b
CannonWilson <CannonGWilson@gmail.com>
2024-03-03 16:29:17 -0600

    Created system for transforming projector image

--- notebooks/helpers.py
+++ notebooks/helpers.py
@@ -5,16 +5,29 @@ used in the various notebook files.
 
 import matplotlib.pyplot as plt
 
-def show_img_in_subplots(images, titles, x, y, figsize):
+def show_img_in_subplots(images, titles, x, y, figsize, cmaps=[]):
     """
     Create a figure of the proposed size x by y.
     Fill it with images in the order they are
-    provided.
+    provided. Sets the color map to be rgb by default.
     """
     _, axes = plt.subplots(x, y, figsize=figsize)
-    for ax, image, title in zip(axes.flatten(), images, titles):
-        ax.imshow(image)
+    if len(cmaps) != len(images):
+        cmaps = ["viridis" for _ in range(len(images))]
+    for i, (ax, image, title) in enumerate(zip(axes.flatten(), images, titles)):
+        ax.imshow(image, cmaps[i])
         ax.set_title(title)
         ax.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+def show_single_img(image, title, cmap="viridis"):
+    """
+    Create a plot for a single image
+    with the given title.
+    """
+    plt.imshow(image, cmap)
+    plt.title(title)
+    plt.axis("off")
     plt.tight_layout()
     plt.show()
